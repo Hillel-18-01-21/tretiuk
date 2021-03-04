79d2c67774e9eedd916533623ab915485838195d
multicalor <d.tretiuk@gmil.com>
2021-03-04 10:53:44 +0200

    hw_10

--- home_work_10/js/script.js
+++ home_work_10/js/script.js
@@ -1,31 +1,28 @@
-function calc (closedNum) {
-  return  {
+function calc(closedNum) {
+  return {
     add: function (nextNum) {
       closedNum += nextNum;
     },
-    sub:function (nextNum) {
+    sub: function (nextNum) {
       closedNum -= nextNum;
     },
-    div:function (nextNum){
+    div: function (nextNum) {
       closedNum /= nextNum;
     },
-    mult:function (nextNum) {
+    mult: function (nextNum) {
       closedNum *= nextNum;
     },
-    getResult:function () {
+    getResult: function () {
       return closedNum;
     },
-  }
+  };
 }
-  
+
 const first = calc(10);
 
-console.log(first.add(10))
-console.log(first.sub(5))
-console.log(first.div(10))
-console.log(first.mult(100))
-console.log(first.getResult())
-console.dir(first)
-
-
-
+console.log(first.add(10));
+console.log(first.sub(5));
+console.log(first.div(10));
+console.log(first.mult(100));
+console.log(first.getResult());
+console.dir(first);
